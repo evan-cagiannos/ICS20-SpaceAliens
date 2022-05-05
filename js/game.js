ae3dfa7e357130c30d2749e33c0935b701afc8e5
Evan Cagiannos <99355231+evan-cagiannos@users.noreply.github.com>
2022-05-05 16:37:48 +0000

    Background Fixes

--- js/game.js
+++ js/game.js
@@ -1,47 +1,49 @@
 /* global Phaser */
 
-// Copyright (c) 2022 Evan Cagiannos All rights reserverd.
-//
-// Created by: Zaida Hammel
-// Created on: April 2022
+// Created by: Evan Cagiannos
+// Created on: May 2022
 // This is the Phaser3 configuration file
 
-// This scene imports statements
-import SplashScene from "./splashScene.js";
-import TitleScene from "./titleScene.js";
+import SplashScene from './splashScene.js'
+import TitleScene from './titleScene.js'
+import MenuScene from './menuScene.js'
+import GameScene from './gameScene.js'
 
-// Our game scene
-const splashScene = new SplashScene();
-const titleScene = new TitleScene();
+// Our game scenes
+const splashScene = new SplashScene()
+const titleScene = new TitleScene()
+const menuScene = new MenuScene()
+const gameScene = new GameScene()
 
-/**
- * Start Phaser Game.
- */
+/** Game scene.*/
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
   physics: {
-    default: "arcade",
+    default: 'arcade',
     arcade: {
       debug: true,
     },
   },
   // set background color
-  backgroundColor: 0x5f6e7a,
+  backgroundColor: 0xffffff,
   scale: {
     mode: Phaser.Scale.FIT,
-    // place it in the middle of the page
+    // we place it in the middle of the page.
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
-};
+}
 
-const game = new Phaser.Game(config);
-// console.log (game)
+const game = new Phaser.Game(config)
+// console.log(game)
 
 // load scenes
-game.scene.add("splashScene", splashScene);
-game.scene.add("titleScene", titleScene);
+// Note: remember any 'key' is global and CAN NOT be reused!
+game.scene.add('splashScene', splashScene)
+game.scene.add('titleScene', titleScene)
+game.scene.add('menuScene', menuScene)
+game.scene.add('gameScene', gameScene)
 
-// start title
-game.scene.start("splashScene");
+// the start scene
+game.scene.start('splashScene')
--- js/splashscene,.js
+++ js/splashscene,.js
@@ -1,44 +1,67 @@
 /* global Phaser */
 
-// Copyright (c) 2022 Evan Cagiannos All rights reserved
-//
 // Created by: Evan Cagiannos
-// Created on: April 2022
-// This is the splash scene
+// Created on: May 2022
+// This is the Splash Scene
 
 /**
- * This class is the splash scene.
+ * This class is the Splash Scene.
  */
-class SplashScene extends Phaser.Scene {
+ class SplashScene extends Phaser.Scene {
+  /**
+   * This method is the construtor.
+   */
   constructor() {
-    super({ key: "splashScene" });
+    super({ key: 'splashScene' })
 
-    this.splashSceneBackgroundImage = null;
+    this.splashSceneBackgroundImage = null
   }
 
+  /**
+   * Can be defined on your own Scenes.
+   * This method is called by the Scene Manager when the scene starts,
+   *   before preload() and create().
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   */
   init(data) {
-    this.cameras.main.setBackgroundColor("#1e0032");
+      this.cameras.main.setBackgroundColor('#ffffff')
   }
 
+  /**
+   * Can be defined on your own Scenes.
+   * Use it to load assets.
+   */
   preload() {
-    console.log("Splash Scene");
-    this.load.image("splashSceneBackground", "./assets/splashSceneImage.png");
+    console.log('Splash Scene')
+    this.load.image('splashSceneBackground', './assets/splashSceneImage.png')
   }
 
+  /**
+   * Can be defined on your own Scenes.
+   * This method is called by the Scene Manager when the scene starts,
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   */
   create(data) {
     this.splashSceneBackgroundImage = this.add.sprite(
       0,
       0,
-      "splashSceneBackground"
-    );
-    this.splashSceneBackgroundImage.x = 1920 / 2;
-    this.splashSceneBackgroundImage.y = 1080 / 2;
+      'splashSceneBackground'
+    )
+    this.splashSceneBackgroundImage.x = 1920 / 2
+    this.splashSceneBackgroundImage.y = 1080 / 2
   }
 
+  /**
+   * Should be overridden by your own Scenes.
+   * This method is called once per game step while the scene is running.
+   *  @param {number} time - The current time.
+   *  @param {number} delta - The delta time in ms since the last frame.
+   */
   update(time, delta) {
     if (time > 3000) {
-      this.scene.switch("titleScene");
+    this.scene.switch('titleScene')
     }
   }
 }
-export default SplashScene;
+
+export default SplashScene
